ec28d3140ab76a9e6603856b948edce41380972a
simonzyWu <simonzywu@Simonzys-Air.local>
2025-05-18 15:04:45 +0800

    feat: 接入mailtrap实现邮箱发送功能

---
+++ backend/mailtrap/mailtrap.config.js
@@ -0,0 +1,27 @@
+const { MailtrapClient } = require("mailtrap");
+
+const TOKEN = "96f207612ccd9e1ef04e362166826f07";
+
+const client = new MailtrapClient({
+  token: TOKEN,
+});
+
+const sender = {
+  email: "hello@demomailtrap.co",
+  name: "Mailtrap Test",
+};
+const recipients = [
+  {
+    email: "simonwuzy412@gmail.com",
+  }
+];
+
+client
+  .send({
+    from: sender,
+    to: recipients,
+    subject: "You are awesome!",
+    text: "Congrats for sending test email with Mailtrap!",
+    category: "Integration Test",
+  })
+  .then(console.log, console.error);
