b8a73e0ca81c371162a4d43c0596a50bb1e21667
simonzyWu <simonzywu@Simonzys-Air.local>
2025-05-18 14:13:21 +0800

    feat: 测试注册接口

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -31,8 +31,17 @@ export const signup = async (req, res) => {
 
         await user.save();
 
-        // jwt 
-        generateTokenAndSetCookie(res.user_id)
+        // jwt 鉴权
+        generateTokenAndSetCookie(res, res.user_id);
+
+        res.status(201).json({
+            success: true,
+            message: "User created successfully",
+            user:{
+                ...user._doc,
+                password: undefined,
+            }
+        })
     } catch (err) {
         res.status(400).json({
             success: false,
--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -4,4 +4,14 @@ export const generateTokenAndSetCookie = (res, userId) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
         expiresIn: "7d",
     });
+
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        // 预防 csrf 攻击
+        sameSite: "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    })
+
+    return token;
 }
